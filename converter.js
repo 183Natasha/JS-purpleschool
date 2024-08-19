7d616d3514305fc55261decf401f9d4f20c59ab3
183Natasha <183natasha@mail.ru>
2024-08-19 14:20:43 +0000

    start

---
+++ converter.js
@@ -0,0 +1,14 @@
+// Создать функцию конвертации валют, принимающую три параметра:
+// Сумма для конвертации
+// Исходная валюта
+// Целевая валюта
+
+let balance = 1000;
+
+const Rubl_To_USD = 0.011168 ;
+const USD_To_Rubl = 89.54;
+
+function converter (balance, tipeStart, tipeEnd){
+    
+}
+
