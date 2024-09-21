ea3496e4ff3d279f2a3dacfec6c1f6618b06b794
183Natasha <183natasha@mail.ru>
2024-09-21 06:34:50 +0000

    реализована query

--- 13-query/query.js
+++ 13-query/query.js
@@ -1,7 +1,15 @@
-
-
-{search: "Вася", take: 10}
-
-function query(){
-    
+let user = {
+    search: "Вася",
+    take: 10,
+    query: function () {
+        let arr = [];
+        for (let key in this) {
+            if (key !== "query") {
+                arr.push(`${key}=${(this[key])}`)
+            }
+        }
+        return (arr.join('&'))
+    }
 }
+
+console.log(user.query())
